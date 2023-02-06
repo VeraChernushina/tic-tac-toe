a7a1480f3c5cb36de4f224427a423678def2e1bd
Vera Chernushina <chernushina.vera@gmail.com>
2023-02-06 13:31:16 +0300

    feat: simple styles

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,128 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <h1>Tic tac toe</h1>
+    <div class="play-area">
+      <div id="block_0" class="block"></div>
+      <div id="block_1" class="block"></div>
+      <div id="block_2" class="block"></div>
+      <div id="block_3" class="block"></div>
+      <div id="block_4" class="block"></div>
+      <div id="block_5" class="block"></div>
+      <div id="block_6" class="block"></div>
+      <div id="block_7" class="block"></div>
+      <div id="block_8" class="block"></div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.container {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #eee;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+h1 {
+  font-size: 5rem;
+  margin-bottom: 0.5em;
+}
+h2 {
+  margin-top: 1em;
+  font-size: 2rem;
+  margin-bottom: 0.5em;
+}
+.play-area {
+  display: grid;
+  width: 300px;
+  height: 300px;
+  grid-template-columns: auto auto auto;
+}
+.block {
+  display: flex;
+  width: 100px;
+  height: 100px;
+  align-items: center;
+  justify-content: center;
+  font-size: 3rem;
+  font-weight: bold;
+  border: 3px solid black;
+  transition: background 0.2s ease-in-out;
+}
+.block:hover {
+  cursor: pointer;
+  background: #0ff30f;
+}
+.occupied:hover {
+  background: #ff3a3a;
+}
+.win {
+  background: #0ff30f;
+}
+.win:hover {
+  background: #0ff30f;
+}
+#block_0,
+#block_1,
+#block_2 {
+  border-top: none;
+}
+#block_0,
+#block_3,
+#block_6 {
+  border-left: none;
+}
+#block_6,
+#block_7,
+#block_8 {
+  border-bottom: none;
+}
+#block_2,
+#block_5,
+#block_8 {
+  border-right: none;
+}
+button {
+  outline: none;
+  border: 4px solid green;
+  padding: 10px 20px;
+  font-size: 1rem;
+  font-weight: bold;
+  background: none;
+  transition: all 0.2s ease-in-out;
+}
+button:hover {
+  cursor: pointer;
+  background: green;
+  color: white;
+}
+.playerWin {
+  color: green;
+}
+.computerWin {
+  color: red;
+}
+.draw {
+  color: orangered;
+}
+@media only screen and (max-width: 600px) {
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 0.5em;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  h2 {
+    margin-top: 1em;
+    font-size: 1.3rem;
   }
 }
 </style>
